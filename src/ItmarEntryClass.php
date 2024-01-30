78d7f26ac17dfbecaeb657ec89201a8266ad5d18
itmaroon <master@itmaroon.net>
2024-01-30 20:21:28 +0900

    1-30 second commit

--- src/ItmarEntryClass.php
+++ src/ItmarEntryClass.php
@@ -1,4 +1,6 @@
 <?php
+namespace Itmar\BlockClassPakage;
+
 class ItmarEntryClass {
   function block_init($text_domain)
   {
@@ -35,48 +37,5 @@ class ItmarEntryClass {
 
     //PHP用のテキストドメインの読込（国際化）
     load_plugin_textdomain( $text_domain, false, basename( dirname( __FILE__ ) ) . '/languages' );
-  }
-
-  function add_enqueue() {
-    //jquery-easingを読み込む
-    if (!wp_script_is('itmar_jquery_easing', 'enqueued')) {
-      wp_enqueue_script( 'itmar_jquery_easing', plugins_url('assets/jquery.easing.min.js', __FILE__ ), array('jquery' ), true );
-    }
-    //vegasを読み込む
-    if (!wp_script_is('itmar_vegas_js', 'enqueued')) {
-      wp_enqueue_script('itmar_vegas_js', plugins_url('assets/vegas.min.js', __FILE__ ), array('jquery'), true);
-    }
-    
-    if (!wp_style_is('itmar_vegas_css', 'enqueued')) {
-        wp_enqueue_style('itmar_vegas_css', plugins_url('assets/vegas.min.css', __FILE__));
-    }
-
-    //swiperを読み込む
-    if (!wp_script_is('itmar_swiper_js', 'enqueued')) {
-      wp_enqueue_script('itmar_swiper_js', plugins_url('assets/swiper-bundle.min.js', __FILE__ ), array('jquery'), true);
-    }
-    
-    if (!wp_style_is('itmar_swiper_css', 'enqueued')) {
-        wp_enqueue_style('itmar_swiper_css', plugins_url('assets/swiper-bundle.min.css', __FILE__));
-    }
-  }
-
-  function add_front_enqueue(){
-    if(!is_admin()){
-      //独自jsのエンキュー
-      $script_path = plugin_dir_path(__FILE__) . 'assets/mvBlocks.js';
-      wp_enqueue_script(
-        'itmar_mv_blocks_js',
-        plugins_url('/assets/mvBlocks.js', __FILE__),
-        array('jquery','wp-i18n'),
-        filemtime($script_path),
-        true
-      );
-      //jsで使えるようにhome_urlをローカライズ
-      wp_localize_script('itmar_mv_blocks_js', 'itmar_block_option', array(
-        'home_url' => home_url(),
-        'plugin_url' => plugins_url('', __FILE__)
-      ));
-    }
   }
 }
