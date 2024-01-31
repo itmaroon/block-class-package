83b2d961c68b7aa59bebf44640c467e580bf2ca4
itmaroon <master@itmaroon.net>
2024-01-31 14:12:06 +0900

    1-31 3rd

--- src/ItmarEntryClass.php
+++ src/ItmarEntryClass.php
@@ -2,17 +2,17 @@
 namespace Itmar\BlockClassPakage;
 
 class ItmarEntryClass {
-  function block_init($text_domain, $plugin_path)
+  function block_init($text_domain, $file_path)
   {
     //ブロックの登録
-    foreach (glob($plugin_path . 'build/blocks/*') as $block) {
+    foreach (glob(plugin_dir_path($file_path) . 'build/blocks/*') as $block) {
       $block_name = basename($block);
       $script_handle = 'itmar-handle-' . $block_name;
-      $script_file = $plugin_path . 'build/blocks/'.$block_name.'/index.js';
+      $script_file = plugin_dir_path($file_path) . 'build/blocks/'.$block_name.'/index.js';
       // スクリプトの登録
       wp_register_script(
         $script_handle,
-        plugins_url( 'build/blocks/'.$block_name.'/index.js', $plugin_path ),
+        plugins_url( 'build/blocks/'.$block_name.'/index.js', $file_path ),
         array( 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-block-editor' ),
         filemtime($script_file)
       );
@@ -26,16 +26,17 @@ class ItmarEntryClass {
       );
       
       // その後、このハンドルを使用してスクリプトの翻訳をセット
-      wp_set_script_translations( $script_handle, $text_domain, $plugin_path . 'languages' );
+      wp_set_script_translations( $script_handle, $text_domain, plugin_dir_path($file_path) . 'languages' );
       //jsで使えるようにhome_urlをローカライズ
-      wp_localize_script($script_handle, 'itmar_block_option', array(
+      $js_name = str_replace("-", "_", $text_domain);
+      wp_localize_script($script_handle, $js_name, array(
         'home_url' => home_url(),
-        'plugin_url' => plugins_url('', $plugin_path)
+        'plugin_url' => plugins_url('', $file_path)
       ));
       
     }
 
     //PHP用のテキストドメインの読込（国際化）
-    load_plugin_textdomain( $text_domain, false, basename( dirname( $plugin_path ) ) . '/languages' );
+    load_plugin_textdomain( $text_domain, false, basename( dirname( $file_path ) ) . '/languages' );
   }
 }
